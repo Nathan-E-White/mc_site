df9f10f3c793d9a5a0beb863e4c335055c8a428d
Nathan E White <nwnn4@mail.missouri.edu>
2020-11-27 13:40:24 -0600

    Added Dockerfile section headers for expanded documentation.
    Added npm_run_export.bash file with npm script and simple documentation.
    
    Added npm_start.bash file with npm script and simple documentation.

---
+++ scripts/npm_start.bash
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+# =========================================================================== #
+# Description:  The following command starts a simple Node.js server located
+#               at `__sapper__/build/index.js`. Static assets are served up
+#               from the roots `static` directory.
+# =========================================================================== #
+
+npm run build;
