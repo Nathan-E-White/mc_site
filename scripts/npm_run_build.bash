a567e81a334639d72ab82ae88e01ff12a1df0b1c
Nathan E White <nwnn4@mail.missouri.edu>
2020-11-27 13:37:56 -0600

    Added Dockerfile section headers for expanded documentation.

---
+++ scripts/npm_run_build.bash
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+# =========================================================================== #
+# Description:  The following command outputs artifacts to `__sapper__/build`
+# =========================================================================== #
+
+npm run build;
