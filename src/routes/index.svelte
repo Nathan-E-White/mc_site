59425111b47b112e3cb0800a5d3ff80436e20d7b
Nathan E White <nwnn4@mail.missouri.edu>
2020-11-28 04:12:55 -0600

    Added a Svelte component for the contact us page, in contact.svelte, and added a line in the Nav component to put a link to the contact page in the tab.
    Changed the template default to a 'Page coming soon' message.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,14 +33,14 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Mogul Charts -- Trade Like a Mogul</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Check back later...</h1>
 
 <figure>
 	<img alt='Borat' src='great-success.png'>
 	<figcaption>HIGH FIVE!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p><strong>Page coming soon!</strong></p>
