39a79fa64a30ae2f20f106784fc3ab71700d6461
Nathan E White <nwnn4@mail.missouri.edu>
2020-11-28 17:52:00 -0600

    Added in some metadata and open graph tags in the header for the contact file.

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,5 +1,14 @@
 <svelte:head>
     <title>Contact</title>
+    <meta name="description" content="Contact information including... for MogulChart.com">
+    <meta name="keywords" content="contact email phone address">
+
+    <!-- Open Graph -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://mogulchart.com/contact">
+    <meta property="og:title" content="Contact">
+    <meta property="og:description" content="Contact information for MogulChart.com">
+
 </svelte:head>
 
 <p>Placeholder for contact information.</p>
