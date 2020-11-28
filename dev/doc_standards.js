ae15d0efce2f29c7fb80f1b241f3d7e58b2126b3
Nathan E White <nwnn4@mail.missouri.edu>
2020-11-28 17:41:56 -0600

    Created doc_standards.js, this file will (eventually) have copy-and-pastable JSDoc comments that developers can use as templates for files, classes, functions, etc. to maintain documentation standards.

---
+++ dev/doc_standards.js
@@ -0,0 +1,60 @@
+
+
+/** ---- FOR A FUNCTION ----
+ *  @summary            Brief one liner
+ *  @description        Longer supplemental description
+ *  @deprecated         x.x.x   If applicable, provide version function was deprecated, indicate new function.
+ *  @since              x.x.x   Describe significant changes made at major versions..
+ *  @access             If function is private or for internal use
+ *  @class              Used for class constructors
+ *  @augments           For class constructors, list direct parents
+ *  @mixes              List mixins that are mixed into object
+ *  @alias              If function is first assigned to a temporary variable, allows you to change the name it's doc.
+ *  @memberOf           Namespace of function if JSDoc is unable to resolve
+ *  @static             used to mark a function is a static method on class constructor
+ *  @see                Points to a function or class that is relied on
+ *  @link               URL that points to more information
+ *  @fires              Event fired by the function. Events tied to a specific class should list the class name
+ *  @listens            Event listened for by function. Event must be prefixed with event namespace or class name
+ *  @global             Marks that function will be in global namespace
+ *  @param              Give a brief description of the variable, note if optional, default.
+ *  @yield              For generator funcs, description of values yielded from function.
+ *  @return             Describes what the function returns
+ */
+
+/* COPY AND PASTE THE FOLLOWING */
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @since      x.x.x
+ * @deprecated x.x.x Use new_function_name() instead.
+ * @access     private
+ *
+ * @class
+ * @augments parent
+ * @mixes    mixin
+ *
+ * @alias    realName
+ * @memberof namespace
+ *
+ * @see  Function/class relied on
+ * @link URL
+ * @global
+ *
+ * @fires   eventName
+ * @fires   className#eventName
+ * @listens event:eventName
+ * @listens className~event:eventName
+ *
+ * @param {type}   var           Description.
+ * @param {type}   [var]         Description of optional variable.
+ * @param {type}   [var=default] Description of optional variable with default variable.
+ * @param {Object} objectVar     Description.
+ * @param {type}   objectVar.key Description of a key in the objectVar parameter.
+ *
+ * @yield {type} Yielded value description.
+ *
+ * @return {type} Return value description.
+ */
