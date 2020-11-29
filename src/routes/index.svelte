5396ecec661c613cec253c32f09e4c2344e09b84
Nathan E White <nwnn4@mail.missouri.edu>
2020-11-28 21:44:02 -0600

    Filled in OpenGraph meta tags.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,21 +33,18 @@
 </style>
 
 <svelte:head>
-    <title>Mogul Charts -- Trade Like a Mogul</title>
+    <title>Mogul Charts -- Trade Like a Mogul!</title>
 
     <!-- Mandatory OpenGraph tags -->
-    <meta property="og:title" content="">
+    <meta property="og:title" content="Mogul Charts -- Trade Like a Mogul!">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="">
+    <meta property="og:url" content="https://www.mogulchart.com">
     <meta property="og:image" content="">
 
     <!-- Optional OpenGraph tags -->
-    <meta property="og:audio" content="">
-    <meta property="og:description" content="">
-    <meta property="og:locale" content="">
-    <meta property="og:locale:alternate" content="">
-    <meta property="site_name" content="">
-    <meta property="video" content="">
+    <meta property="og:description" content="MogulChart.com is your first and only stop for real time market data and cutting edge quantitative analysis.">
+    <meta property="og:locale" content="en_US">
+    <meta property="site_name" content="Mogul Charts">
 </svelte:head>
 
 <h1>Check back later...</h1>
