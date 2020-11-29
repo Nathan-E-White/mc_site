ac45baa7719b6e3544b561c7b3012cdb68edf662
Nathan E White <nwnn4@mail.missouri.edu>
2020-11-28 18:11:06 -0600

    hidden-links.svelte will sort of serve as a hidden nav page for the site, will have to have a small link to it somewhere.

---
+++ src/routes/hidden-links.svelte
@@ -0,0 +1,4 @@
+<h1>Hidden Links</h1>
+<p>These are links to different pages on the site that are accessible, but not any sort of thing users would look for unless they specifically wanted to find it.</p>
+
+<a href="/sitemap.xml">XML Sitemap</a><br/>
