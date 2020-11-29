feae8d5dca79f854c65bc68da794f69c2552413e
Nathan E White <nwnn4@mail.missouri.edu>
2020-11-28 21:38:42 -0600

    Filled in OpenGraph meta tags.

--- src/routes/disclaimer.svelte
+++ src/routes/disclaimer.svelte
@@ -2,18 +2,15 @@
     <title>Disclaimers</title>
 
     <!-- Mandatory OpenGraph tags -->
-    <meta property="og:title" content="">
+    <meta property="og:title" content="Disclaimers">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="">
+    <meta property="og:url" content="https://www.mogulchart.com/disclaimer">
     <meta property="og:image" content="">
 
     <!-- Optional OpenGraph tags -->
-    <meta property="og:audio" content="">
-    <meta property="og:description" content="">
-    <meta property="og:locale" content="">
-    <meta property="og:locale:alternate" content="">
-    <meta property="site_name" content="">
-    <meta property="video" content="">
+    <meta property="og:description" content="Legal disclaimer for users of Mogul Charts that use the site in ways for which it was not designed.">
+    <meta property="og:locale" content="en_US">
+    <meta property="site_name" content="Mogul Charts">
 </svelte:head>
 
 
