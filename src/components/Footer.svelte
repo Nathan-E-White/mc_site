72e31685d0ae853a92f0ba1d17056a181f980682
Nathan E White <nwnn4@mail.missouri.edu>
2020-12-02 09:39:15 -0600

    Began to migrate some stuff to the new page-footer.

---
+++ src/components/Footer.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let footer;
+    export let segment;
+</script>
+
+
+<section id="page-footer">
+    <nav>
+        <ul>
+
+            <!-- Home page -->
+            <li><a rel=prefetch aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
+
+            <!-- About page -->
+            <li><a rel=prefetch aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
+
+            <!-- Contact Us Page -->
+            <li><a rel=prefetch aria-current='{segment === "contact" ? "page" : undefined}' href='contact'>contact</a></li>
+
+            <!-- Blogs Page -->
+            <li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li>
+        </ul>
+    </nav>
+</section>
