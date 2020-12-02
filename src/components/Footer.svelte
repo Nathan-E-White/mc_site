71254c2f3a4658ca8a6cfa35445b028213e0317d
Nathan E White <nwnn4@mail.missouri.edu>
2020-12-02 09:46:00 -0600

    Removed link to home and to blog from the footer.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -8,17 +8,12 @@
     <nav>
         <ul>
 
-            <!-- Home page -->
-            <li><a rel=prefetch aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
-
             <!-- About page -->
             <li><a rel=prefetch aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
 
             <!-- Contact Us Page -->
             <li><a rel=prefetch aria-current='{segment === "contact" ? "page" : undefined}' href='contact'>contact</a></li>
 
-            <!-- Blogs Page -->
-            <li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li>
         </ul>
     </nav>
 </section>
