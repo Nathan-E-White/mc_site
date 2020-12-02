20950edf124464a8d46d307108f61f8888e9023e
Nathan E White <nwnn4@mail.missouri.edu>
2020-12-02 10:14:10 -0600

    Added more links to other site pages into the footer.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -8,12 +8,82 @@
     <nav>
         <ul>
 
-            <!-- About page -->
-            <li><a rel=prefetch aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
+            <!---- About page ---->
+            <li><a rel="prefetch noreferrer noopener"
+                   aria-current='{segment === "about" ? "page" : undefined}'
+                   href='about'
+                   hreflang="en"
+                   target="_self">
+                About</a>
+            </li>
 
-            <!-- Contact Us Page -->
-            <li><a rel=prefetch aria-current='{segment === "contact" ? "page" : undefined}' href='contact'>contact</a></li>
+            <!---- Contact Us Page ---->
+            <li><a rel="prefetch noreferrer noopener"
+                   aria-current='{segment === "contact" ? "page" : undefined}'
+                   href='contact'
+                   hreflang="en"
+                   target="_self">
+                Contact
+                </a>
+            </li>
 
+            <!---- Cookies Policy ---->
+            <li><a rel="prefetch noreferrer noopener"
+                   aria-current='{segment === "cookies" ? "page" : undefined}'
+                   href="cookies-policy"
+                   hreflang="en"
+                   target="_self">
+                Cookies Policy</a>
+            </li>
+
+            <!-- Disclaimers -->
+            <li><a rel="prefetch noreferrer noopener"
+                   aria-current='{segment === "disclaimers" ? "page" : undefined}'
+                   href="Disclaimers"
+                   hreflang="en"
+                   target="_self"
+            >
+                Legal Stuff</a>
+            </li>
+
+            <!-- FAQs -->
+            <li><a rel="prefetch noreferrer noopener"
+                   aria-current='{segment === "faqs" ? "page" : undefined}'
+                   href="faqs"
+                   hreflang="en"
+                   target="_self">
+                FAQs</a>
+            </li>
+
+            <!-- Privacy Policy -->
+            <li><a rel="prefetch noreferrer noopener"
+                   aria-current="{segment === 'privacy' ? 'page' : undefined}"
+                   href="privacy-policy">
+                Privacy Policy</a>
+            </li>
+
+            <!-- Site Status -->
+            <li><a rel="prefetch noreferrer noopener"
+                   aria-current="{segment === 'status' ? 'page' : undefined}"
+                   href="status"
+                   hreflang="en"
+                   target="_self">
+                Site Status
+            </a>
+            </li>
+
+            <!-- ========================================================== -->
+            <!--                         Terms of Use                       -->
+            <!-- ========================================================== -->
+            <li><a
+                    rel="prefetch noreferrer noopener"
+                    aria-current="{segment === 'terms' ? 'page' : undefined}"
+                    href="terms-of-use"
+                    hreflang="en"
+                    target="_self">
+                Terms of Use
+            </a>
+            </li>
         </ul>
     </nav>
 </section>
