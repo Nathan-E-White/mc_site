0d8ea792a29b1a7c5cda1e7b360f341ee08b1ba0
Nathan E White <nwnn4@mail.missouri.edu>
2020-12-02 15:04:33 -0600

    Added a <h1> level outline of terms of use.

--- src/routes/TermsOfUse.svelte
+++ src/routes/TermsOfUse.svelte
@@ -17,4 +17,18 @@
 </svelte:head>
 
 
-<p><strong>Check back later...</strong></p>
+<svelte:body>
+    <h1>Changes to Terms of Use</h1>
+    <h1>Changes to the Website</h1>
+    <h1>Ownership of Information</h1>
+    <h1>License</h1>
+    <h1>Redistribution</h1>
+    <h1>Attribution</h1>
+    <h1>Third Party Entities</h1>
+    <h1>Privacy Policy</h1>
+    <h1>Cookie Policy</h1>
+    <h1>Security</h1>
+    <h1>Feedback</h1>
+    <h1>Indemnification</h1>
+    <h1>Termination</h1>
+</svelte:body>
