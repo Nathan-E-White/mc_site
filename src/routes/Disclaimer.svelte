97982b388c02a0ea0b9e34dc90b0ca128ff1cf27
Nathan E White <nwnn4@mail.missouri.edu>
2020-12-05 15:06:59 -0600

    Added in a few disclaimers, more to add later.

--- src/routes/Disclaimer.svelte
+++ src/routes/Disclaimer.svelte
@@ -8,14 +8,79 @@
     <meta property="og:image" content="">
 
     <!-- Optional OpenGraph tags -->
-    <meta property="og:description" content="Legal disclaimer for users of Mogul Charts that use the site in ways for which it was not designed.">
+    <meta property="og:description"
+          content="Legal disclaimer for users of Mogul Charts that use the site in ways for which it was not designed.">
     <meta property="og:locale" content="en_US">
     <meta property="site_name" content="Mogul Charts">
 </svelte:head>
 
 
 <svelte:body>
-    <h1>Content on Site</h1>
-    <h1>Investment Advice</h1>
-    <h1>Past & Hypothetical Performance</h1>
+
+    <article>
+
+        <header>
+            <h1>About Disclaimers</h1>
+            <p>These are general pieces of advice meant for users to take into consideration as they use the site.</p>
+            <p>They are meant to be much more general than the <a href="/terms-of-use">Terms of Use</a> but are still
+                meant to be taken seriously.</p>
+            <p>Unlike the <a href="/code-of-conduct">Code of Conduct</a> these disclaimers are intended to manage
+                expectations, not behavior.</p>
+        </header>
+
+        <section>
+            <h1>Content on Site</h1>
+            <p>...</p>
+        </section>
+
+        <section>
+            <h1>No Investment Advice</h1>
+            <p>
+                The Content of the Site is for informational purposes only. Although the content of the site is meant to
+                be informational, informative, entertaining and educational, you should not mistake any information or
+                material found here as legal, tax, investment, financial planning or other professional advice.
+            </p>
+            <p>
+                Nothing contained on the Site is meant to constitute a specific solicitation, recommendation,
+                endorsement or offer by or for any person to buy or sell any financial instrument, security or
+                investment.
+            </p>
+        </section>
+
+        <section>
+            <h1>Risks of Investing</h1>
+            <p>
+                Different classes of investments including but not limited to items such as stocks, bonds, options,
+                currencies all involve their own peculiar risks of loss. It is in general, always possible to experience
+                loss of principal due to an inability to mitigate all risks.</p>
+            <p>
+                Some investments use leverage, which will accelerate gains and losses and may induce losses greater than
+                that of the principal investment.</p>
+            <p>
+                Investment in foreign entities involves special risks such as greater volatility due to uncertainty in
+                political, economic and currency and accounting issues.
+            </p>
+        </section>
+
+        <section>
+            <h1>Past & Hypothetical Performance</h1>
+            <p>
+                The past performance of a security or company is not a guarantee or predictor of future investment
+                performance. This site contains speculative discussions of future investment performance. These
+                speculations are for research purposes only.
+            </p>
+            <p>
+                The performance of a security, company or investment in a backtest or other simulation is hypothetical.
+                These are not actual records of trades that were completed, they are estimations of what could have
+                happened if one had behaved accordingly. As noted above, historical performance is not always a reliable
+                indicator of future performance and the same must be said about hypothetical performance as well.
+            </p>
+        </section>
+
+        <section>
+            <h1>Advertisements</h1>
+            <p></p>
+        </section>
+
+    </article>
 </svelte:body>
