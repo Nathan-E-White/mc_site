7bddcd89777bad9888a02d68047f2f8535d9a3d1
Nathan E White <nwnn4@mail.missouri.edu>
2020-12-05 15:33:27 -0600

    Filled in a lot of bare bones links and a brief description of what each page does.

--- src/routes/SiteMap.svelte
+++ src/routes/SiteMap.svelte
@@ -1,19 +1,114 @@
 <svelte:head>
-    <title>Hidden Links</title>
+    <title>Mogul Charts -- Site Map</title>
 
     <!-- Mandatory OpenGraph tags -->
-    <meta property="og:title" content="Hidden Links">
+    <meta property="og:title" content="Site Map">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="https://www.mogulcharts.com/hidden-links">
+    <meta property="og:url" content="https://www.mogulcharts.com/site-map">
     <meta property="og:image" content="">
 
     <!-- Optional OpenGraph tags -->
-    <meta property="og:description" content="Links to hidden pages on Mogul charts, for crawlers and internet sleuths.">
+    <meta property="og:description" content="Links to pages on Mogul charts, for crawlers and internet sleuths.">
     <meta property="og:locale" content="en_US">
     <meta property="site_name" content="Mogul charts">
 </svelte:head>
 
-<p>
-    These are links to different pages on the site that are accessible, but not any sort of thing most users would
-    look for unless they specifically wanted to find it.
-</p>
+<svelte:body>
+    <article>
+        <header>
+            <h1>About the Sitemap.</h1>
+            <p>
+                The sitemap is a formal description of the structure of the website. It contains links to pages that
+                users frequently visit, as well as links to some more obscure features that may be of interest to web
+                crawlers and developers, but that regular users won't care to necessarily see.
+            </p>
+        </header>
+
+        <section>
+            <h1>Site Basic Features</h1>
+            <p>
+                The basic features list contains links to pages that practically any business webpage must have to be
+                considered complete.
+            </p>
+
+            <h2>Home Page:</h2>
+            <a href="/">Link:</a>
+            <p>
+                A link to the main page that greets visitors to the site.
+            </p>
+
+            <h2>About Us:</h2>
+            <a href="/about-us">Link:</a>
+            <p>
+                Self explanatory, for users a bit more curious about the site or author's background(s).
+            </p>
+
+            <h2>Contact Us:</h2>
+            <a href="/contact-us">Link:</a>
+            <p>
+                Anyone needed basic contact information from us may find it on this page.
+            </p>
+
+            <h2>Feedback:</h2>
+            <a href="/feedback">Link:</a>
+            <p>
+                A place where users can let us know how we're doing, request features, let us know if something's wrong,
+                etc.
+            </p>
+
+            <h2>Frequently Asked Questions:</h2>
+            <a href="/faqs">Link:</a>
+            <p>
+                Some questions frequently asked by visitors and their answers.
+            </p>
+
+            <h2>Site Status</h2>
+            <a href="/site-status">Link:</a>
+            <p>
+                A page that checks if the Site is working at capacity or not.
+            </p>
+
+        </section>
+        <h1>Legal Information</h1>
+        <p>This section contains links to pages on the site that describe various legal policies.</p>
+
+        <h2>Terms of Use</h2>
+        <a href="/terms-of/use">Link:</a>
+        <p>
+            The Terms of Use describe the legal relationship between the site and its users.
+        </p>
+
+        <h2>Code of Conduct</h2>
+        <a href="/code-of-conduct">Link:</a>
+        <p>
+            The Code of Conduct describes the types of user behaviors that are not acceptable and can lead to a ban.
+        </p>
+
+        <h2>Cookies Policy</h2>
+        <a href="/cookies-policy">Link:</a>
+        <p>
+            Information about what cookies are, how we use them, and what control you have over their use on this site.
+        </p>
+
+        <h2>Privacy Policy</h2>
+        <a href="/privacy-policy">Link:</a>
+        <p>
+            Information about what personal information we collect, what we do with it, and what control you have over
+            said collection.
+        </p>
+
+        <h2>Disclaimers</h2>
+        <a href="/disclaimers">Link:</a>
+        <p>
+            Some disclaimers live here. These are more general legal statements that serve more as warnings than
+            specific items in the Terms of Use. Some of these reflect the fact that we know people will use the site in
+            ways that it was not intended to be used.
+        </p>
+
+        <section>
+            <h1>Main Content</h1>
+            <p>This section contains links to subsections that connect the site's main content.</p>
+        </section>
+
+    </article>
+</svelte:body>
