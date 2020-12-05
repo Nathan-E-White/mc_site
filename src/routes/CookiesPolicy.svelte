50d24b545f733a89b391991702d780a0f318701f
Nathan E White <nwnn4@mail.missouri.edu>
2020-12-05 14:58:20 -0600

    Added in some basic information about cookies, where users can manage their settings. Currently we don't use cookies but that will change in the future.

--- src/routes/CookiesPolicy.svelte
+++ src/routes/CookiesPolicy.svelte
@@ -14,5 +14,55 @@
 
 </svelte:head>
 
+<svelte:body>
+    <article>
+        <header>
+            <h1>About the Cookies Policy</h1>
+            <p>This page is meant to serve as a more detailed explanation of the cookies policy found in the
+                <a href="/terms-of-use">Terms of Use</a>, as well as to provide some additional resources for users that
+                are concerned about privacy and tracking issues.
+            </p>
+        </header>
 
-<p><strong>Check back later...</strong></p>
+        <section>
+            <h1>What is a Cookie?</h1>
+            <p>A cookie is a (sometimes) small piece of text consisting of different characters that is backed by a file
+                somewhere on your computer that is accessible to your browser.</p>
+            <p>
+                The purpose of having cookies is that different websites sometimes want to track your browsing
+                experience on their page; either to offer you a more customized experience, or to track you for other
+                purposes. The cookie stores a (sometimes) encoded or encrypted string of characters that the website
+                knows how to interpret as a fact about you; i.e. when was your last login? what time did you arrive on
+                the page? do you have a free or a paid account? etc.
+            </p>
+        </section>
+
+        <section>
+            <h1>What types of cookies are there?</h1>
+            <p>There are session cookies that are transient, and persistent cookies that are stored indefinitely.</p>
+            <p>
+                A session cookie is stored by your browser for the length of time that your web session is active. This
+                could be either a predetermined length of time or until you logout and close your browser. These are
+                commonly used to allow users to navigate between pages without resubmitting information like logging in
+                again, or losing all the items in your cart.
+            </p>
+            <p>
+                A persistent cookie lasts until it is deleted. These cookies are frequently used to retain user
+                preferences for sites that are customizable. They also can be used to identify individual users within
+                the web page. Some marketers place third party cookies in browsers for later identification
+                purposes.
+            </p>
+        </section>
+
+        <section>
+            <h1>How does Mogul Charts use cookies?</h1>
+            <p>We do not currently use cookies at this time.</p>
+        </section>
+
+        <section>
+            <h1>Can I control how my browser stores and processes cookies?</h1>
+            <p>Yes. For more information about how to customizer your browser, please visit <a
+                    href="https://www.aboutcookies.org">AboutCookies.org</a></p>
+        </section>
+    </article>
+</svelte:body>
