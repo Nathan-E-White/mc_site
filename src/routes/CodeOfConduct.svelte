76ef4e2e56fcc00c0d34da5f6f27a186c770c530
Nathan E White <nwnn4@mail.missouri.edu>
2020-12-05 14:57:24 -0600

    Added in a svelte header for CodeOfConduct.svelte; additionally, I added in a summary of conduct, and an outline of sections (boilerplate) to fill in later.

---
+++ src/routes/CodeOfConduct.svelte
@@ -0,0 +1,77 @@
+<svelte:head>
+    <title>Mogul Charts -- User Code of Conduct</title>
+
+    <!-- Mandatory OpenGraph tags -->
+    <meta property="og:title" content="User Code of Conduct">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://www.mogulchart.com/code-of-conduct">
+    <meta property="og:image" content="">
+
+    <!-- Optional OpenGraph tags -->
+    <meta property="og:description" content="The Code of Conduct to govern user behavior on MogulCharts.com">
+    <meta property="og:locale" content="en_US">
+    <meta property="site_name" content="Mogul Charts">
+</svelte:head>
+
+
+<svelte:body>
+
+    <article>
+        <header>
+            <h1>About the Code of Conduct</h1>
+            <p>The code of conduct is the part of the Site's <a href="/terms-of-use">Terms of Use</a> that govern
+                expectations for how users of the Site will conduct themselves.</p>
+        </header>
+
+        <section>
+            <h1>Summary</h1>
+            <p>
+                For a detailed summary, please see the listings located below on this page, but in summary, the code of
+                conduct specifies a few general ideas.
+            </p>
+            <p>
+                First, users must not do anything illegal with the site, either in your jurisdiction or in ours.
+            </p>
+            <p>
+                Second, users must not hack, disrupt, modify or interfere with the operation of the Site or its Content
+                in any way.
+            </p>
+            <p>
+                Third, users must not prevent others from utilizing the Site or its Content.
+            </p>
+            <p>
+                Fourth, users may undertake any effort to systematically reproduce the content or structure of the Site
+                or its data, such as scraping, data mining, etc. It is expected, however, that users will engage in
+                limited reproduction of content for
+                'fair use' purposes, as well as be permitted to reproduce content when specifically noted throughout the
+                Site.
+            </p>
+            <p>
+                Finally, users must treat one another with courtesy and respect; users found to be abusive towards
+                others may be banned or otherwise penalized at the Sites discretion.
+            </p>
+        </section>
+
+        <section>
+            <h1>Unlawful Conduct</h1>
+            <p>You may not use the Site for any purpose that is unlawful. This includes, but is not limited to:</p>
+            <h2>Fraudulant </h2>
+        </section>
+
+
+        <section>
+            <h1>Reproduction of Content and Data</h1>
+            <h2>Web-scraping</h2>
+            <h2>Data-mining</h2>
+        </section>
+
+        <section>
+            <h1>Abusive Conduct</h1>
+            <h2>Interactions with other site members</h2>
+            <h2>Interactions with site operators</h2>
+            <h2>Interactions with third parties</h2>
+        </section>
+
+    </article>
+
+</svelte:body>
