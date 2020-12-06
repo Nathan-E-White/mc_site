ba857b691601b7fce774e370481690ac7241b446
Nathan E White <nwnn4@mail.missouri.edu>
2020-12-05 23:09:57 -0600

    Empty dashboard.svelte file.

--- src/routes/dashboard.svelte
+++ src/routes/dashboard.svelte
@@ -5,3 +5,16 @@
 <body>
 
 </body>
+font-weight: 400;
+--maxWidth: 1284px;
+overflow-wrap: break-word;
+tab-size: 2;
+word-break: normal;
+hyphens: none;
+font-family: Operator Mono A,Operator Mono B,Source Code Pro,Menlo,Consolas,Monaco,monospace;
+white-space: pre;
+font-size: 16px;
+line-height: 1.75;
+box-sizing: border-box;
+color: #5e7671;
+font-style: italic;
