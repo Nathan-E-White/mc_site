64110fe4d42ada7f7d1d700d15832f4b9ab58edb
Nathan E White <nwnn4@mail.missouri.edu>
2020-12-05 23:10:14 -0600

    Empty feedback.svelte file.

--- src/routes/feedback.svelte
+++ src/routes/feedback.svelte
@@ -1,13 +1,17 @@
 <svelte:head>
 
-
 </svelte:head>
 
 
 
-
 <body>
-<h1>Request a Feature</h1>
-<h1>Report a Bug</h1>
-<h1>General Comment</h1>
+
+<!-- -->
+<!-- FORM BEGINS HERE -->
+<!-- -->
+
+
+
+
+
 </body>
