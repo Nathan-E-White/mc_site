e678c76f03ec8e0df745c6e17fb7f3d0cbe2ca50
Nathan E White <nwnn4@mail.missouri.edu>
2020-12-07 18:44:48 -0600

    Put horizontal breaks with <hr> tags in between the rows to balance the page a little bit with some whitespace.

--- src/routes/site-map.svelte
+++ src/routes/site-map.svelte
@@ -25,90 +25,118 @@
     </header>
 
     <section>
-            <h1>Site Basic Features</h1>
-            <p>
-                The basic features list contains links to pages that practically any business webpage must have to be
-                considered complete.
-            </p>
+        <h1>Site Basic Features</h1>
+        <p>
+            The basic features list contains links to pages that practically any business webpage must have to be
+            considered complete.
+        </p>
 
-            <h2>Home Page:</h2>
-            <a href="/">Link:</a>
-            <p>
-                A link to the main page that greets visitors to the site.
-            </p>
+        <hr>
 
-            <h2>About Us:</h2>
-            <a href="/about-us">Link:</a>
-            <p>
-                Self explanatory, for users a bit more curious about the site or author's background(s).
-            </p>
+        <h2>Home Page:</h2>
+        <a href="/">Link:</a>
+        <p>
+            A link to the main page that greets visitors to the site.
+        </p>
 
-            <h2>Contact Us:</h2>
+        <hr>
+
+        <h2>About Us:</h2>
+        <a href="/about-us">Link:</a>
+        <p>
+            Self explanatory, for users a bit more curious about the site or author's background(s).
+        </p>
+
+        <hr>
+
+        <h2>Contact Us:</h2>
         <a href="/contact-us">Link:</a>
-            <p>
-                Anyone needed basic contact information from us may find it on this page.
-            </p>
-
-            <h2>Feedback:</h2>
-            <a href="/feedback">Link:</a>
-            <p>
-                A place where users can let us know how we're doing, request features, let us know if something's wrong,
-                etc.
-            </p>
-
-            <h2>Frequently Asked Questions:</h2>
-            <a href="/faqs">Link:</a>
-            <p>
-                Some questions frequently asked by visitors and their answers.
-            </p>
-
-            <h2>Site Status</h2>
-            <a href="/site-status">Link:</a>
-            <p>
-                A page that checks if the Site is working at capacity or not.
-            </p>
-
-        </section>
-        <h1>Legal Information</h1>
-        <p>This section contains links to pages on the site that describe various legal policies.</p>
-
-        <h2>Terms of Use</h2>
-        <a href="/terms-of-use">Link:</a>
         <p>
-            The Terms of Use describe the legal relationship between the site and its users.
+            Anyone needed basic contact information from us may find it on this page.
         </p>
 
-        <h2>Code of Conduct</h2>
-        <a href="/code-of-conduct">Link:</a>
+        <hr>
+
+        <h2>Feedback:</h2>
+        <a href="/feedback">Link:</a>
         <p>
-            The Code of Conduct describes the types of user behaviors that are not acceptable and can lead to a ban.
+            A place where users can let us know how we're doing, request features, let us know if something's wrong,
+            etc.
         </p>
 
-        <h2>Cookies Policy</h2>
-        <a href="/cookies-policy">Link:</a>
+        <hr>
+
+        <h2>Frequently Asked Questions:</h2>
+        <a href="/faqs">Link:</a>
         <p>
-            Information about what cookies are, how we use them, and what control you have over their use on this site.
+            Some questions frequently asked by visitors and their answers.
         </p>
 
-        <h2>Privacy Policy</h2>
-        <a href="/privacy-policy">Link:</a>
+        <hr>
+
+        <h2>Site Status</h2>
+        <a href="/site-status">Link:</a>
         <p>
-            Information about what personal information we collect, what we do with it, and what control you have over
-            said collection.
+            A page that checks if the Site is working at capacity or not.
         </p>
 
-        <h2>Disclaimers</h2>
-        <a href="/disclaimers">Link:</a>
-        <p>
-            Some disclaimers live here. These are more general legal statements that serve more as warnings than
-            specific items in the Terms of Use. Some of these reflect the fact that we know people will use the site in
-            ways that it was not intended to be used.
-        </p>
+        <hr>
+
+    </section>
+    <h1>Legal Information</h1>
+    <p>This section contains links to pages on the site that describe various legal policies.</p>
+
+    <hr>
+
+    <h2>Terms of Use</h2>
+    <a href="/terms-of-use">Link:</a>
+    <p>
+        The Terms of Use describe the legal relationship between the site and its users.
+    </p>
+
+    <hr>
+
+    <h2>Code of Conduct</h2>
+    <a href="/code-of-conduct">Link:</a>
+    <p>
+        The Code of Conduct describes the types of user behaviors that are not acceptable and can lead to a ban.
+    </p>
+
+    <hr>
+
+    <h2>Cookies Policy</h2>
+    <a href="/cookies-policy">Link:</a>
+    <p>
+        Information about what cookies are, how we use them, and what control you have over their use on this site.
+    </p>
+
+    <hr>
+
+    <h2>Privacy Policy</h2>
+    <a href="/privacy-policy">Link:</a>
+    <p>
+        Information about what personal information we collect, what we do with it, and what control you have over
+        said collection.
+    </p>
+
+    <hr>
+
+    <h2>Disclaimers</h2>
+    <a href="/disclaimers">Link:</a>
+    <p>
+        Some disclaimers live here. These are more general legal statements that serve more as warnings than
+        specific items in the Terms of Use. Some of these reflect the fact that we know people will use the site in
+        ways that it was not intended to be used.
+    </p>
+
+    <hr>
 
     <section>
         <h1>Main Content</h1>
         <p>This section contains links to subsections that connect the site's main content.</p>
     </section>
+
+    <hr>
 
 </article>
 </body>
