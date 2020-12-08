3b64ab74570326e3807d0d5cc8f94b3c8212ec5c
Nathan E White <nwnn4@mail.missouri.edu>
2020-12-07 18:59:56 -0600

    Removed all the unncessary links and unified them with the <h2> tags on the page.

--- src/routes/site-map.svelte
+++ src/routes/site-map.svelte
@@ -25,112 +25,63 @@
     </header>
 
     <section>
+
         <h1>Site Basic Features</h1>
-        <p>
-            The basic features list contains links to pages that practically any business webpage must have to be
-            considered complete.
-        </p>
-
+        <p>The basic pages that describe the site.</p>
         <hr>
 
-        <h2>Home Page:</h2>
-        <a href="/">Link:</a>
-        <p>
-            A link to the main page that greets visitors to the site.
-        </p>
-
+        <h2><a href="/">Home page</a></h2>
+        <p>A link to the main page that greets visitors to the site.</p>
         <hr>
 
-        <h2>About Us:</h2>
-        <a href="/about-us">Link:</a>
-        <p>
-            Self explanatory, for users a bit more curious about the site or author's background(s).
-        </p>
-
+        <h2><a href="/about-us">About Us</a></h2>
+        <p>Self explanatory, for users a bit more curious about the site or author's background(s).</p>
         <hr>
 
-        <h2>Contact Us:</h2>
-        <a href="/contact-us">Link:</a>
-        <p>
-            Anyone needed basic contact information from us may find it on this page.
-        </p>
-
+        <h2><a href="/contact-us">Contact Us</a></h2>
+        <p>Anyone needed basic contact information from us may find it on this page.</p>
         <hr>
 
-        <h2>Feedback:</h2>
-        <a href="/feedback">Link:</a>
-        <p>
-            A place where users can let us know how we're doing, request features, let us know if something's wrong,
-            etc.
-        </p>
-
+        <h2><a href="/feedback">Feedback</a></h2>
+        <p>The place to let us know how we're doing, request a feature or let us know if something's wrong.</p>
         <hr>
 
-        <h2>Frequently Asked Questions:</h2>
-        <a href="/faqs">Link:</a>
-        <p>
-            Some questions frequently asked by visitors and their answers.
-        </p>
-
+        <h2><a href="/faqs">Frequently Asked Questions</a></h2>
+        <p>Some questions frequently asked by visitors and their answers.</p>
         <hr>
 
-        <h2>Site Status</h2>
-        <a href="/site-status">Link:</a>
-        <p>
-            A page that checks if the Site is working at capacity or not.
-        </p>
-
+        <h2><a href="/site-status">Site Status</a></h2>
+        <p>A page that checks if the Site is working at capacity or not.</p>
         <hr>
 
     </section>
-    <h1>Legal Information</h1>
-    <p>This section contains links to pages on the site that describe various legal policies.</p>
+    <section>
 
-    <hr>
+        <h1>Legal Information</h1>
+        <p>This section contains links to pages on the site that describe various legal policies.</p>
+        <hr>
 
-    <h2>Terms of Use</h2>
-    <a href="/terms-of-use">Link:</a>
-    <p>
-        The Terms of Use describe the legal relationship between the site and its users.
-    </p>
+        <h2><a href="/terms-of-use">Terms of Use</a></h2>
+        <p>The Terms of Use describe the legal relationship between the site and its users.</p>
+        <hr>
 
-    <hr>
+        <h2><a href="/code-of-conduct">Code of Conduct</a></h2>
+        <p>The Code of Conduct describes the behaviors that can lead to a site ban.</p>
+        <hr>
 
-    <h2>Code of Conduct</h2>
-    <a href="/code-of-conduct">Link:</a>
-    <p>
-        The Code of Conduct describes the types of user behaviors that are not acceptable and can lead to a ban.
-    </p>
+        <h2><a href="/cookies-policy">Cookies Policy</a></h2>
+        <p>Information about cookies and how we use them.</p>
+        <hr>
 
-    <hr>
+        <h2><a href="/privacy-policy">Privacy Policy</a></h2>
+        <p>Information about privacy on the site.</p>
+        <hr>
 
-    <h2>Cookies Policy</h2>
-    <a href="/cookies-policy">Link:</a>
-    <p>
-        Information about what cookies are, how we use them, and what control you have over their use on this site.
-    </p>
+        <h2><a href="/disclaimers">Disclaimers</a></h2>
+        <p>Generic legal disclaimers about our content.</p>
+        <hr>
 
-    <hr>
-
-    <h2>Privacy Policy</h2>
-    <a href="/privacy-policy">Link:</a>
-    <p>
-        Information about what personal information we collect, what we do with it, and what control you have over
-        said collection.
-    </p>
-
-    <hr>
-
-    <h2>Disclaimers</h2>
-    <a href="/disclaimers">Link:</a>
-    <p>
-        Some disclaimers live here. These are more general legal statements that serve more as warnings than
-        specific items in the Terms of Use. Some of these reflect the fact that we know people will use the site in
-        ways that it was not intended to be used.
-    </p>
-
-    <hr>
-
+    </section>
     <section>
         <h1>Main Content</h1>
         <p>This section contains links to subsections that connect the site's main content.</p>
